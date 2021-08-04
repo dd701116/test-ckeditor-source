cc1daf50dd0c8783d6eee7bacf49675c9b00f683
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2021-08-04 17:54:59 +0200

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# test-ckeditor-source
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Command use to install CKEditor
+
+```
+npm install --save @ckeditor/ckeditor5-vue @ckeditor/ckeditor5-dev-webpack-plugin @ckeditor/ckeditor5-dev-utils postcss-loader@3 raw-loader@0.5.1
+
+npm install --save @ckeditor/ckeditor5-editor-classic @ckeditor/ckeditor5-essentials @ckeditor/ckeditor5-basic-styles   @ckeditor/ckeditor5-link   @ckeditor/ckeditor5-paragraph  @ckeditor/ckeditor5-theme-lark
+
+npm install --save @ckeditor/ckeditor5-upload
+
+npm install --save @ckeditor/ckeditor5-image
+```
---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
+import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
+import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import ImageResizeEditing from '@ckeditor/ckeditor5-image/src/imageresize/imageresizeediting';
+import ImageResizeHandles from '@ckeditor/ckeditor5-image/src/imageresize/imageresizehandles';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      editor: ClassicEditor,
+        editorData: '<p>Content of the editor.</p>',
+        editorConfig: {
+            // The configuration of the editor.
+          plugins: [
+                      EssentialsPlugin,
+                      BoldPlugin,
+                      ItalicPlugin,
+                      LinkPlugin,
+                      ParagraphPlugin,
+                      Base64UploadAdapter,
+                      Image,
+                      ImageCaption,
+                      ImageStyle,
+                      ImageToolbar,
+                      ImageUpload,
+                      ImageResizeEditing,
+                      ImageResizeHandles
+                  ],
+
+                  toolbar: {
+                      items: [
+                          'bold',
+                          'italic',
+                          'link',
+                          'undo',
+                          'redo',
+                          'uploadImage'
+                      ]
+                  },
+                image: {
+                  toolbar: [ {
+                      // Grouping the buttons for the icon-like image styling
+                      // into one drop-down.
+                      name: 'imageStyle:icons',
+                      items: [
+                          'imageStyle:alignLeft',
+                          'imageStyle:alignRight',
+                          'imageStyle:block'
+                      ],
+                      defaultItem: 'imageStyle:block'
+                  },
+                  ]
+              }
+        }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
